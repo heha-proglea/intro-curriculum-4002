e490eecaf565f728e360cbe220fb376432b7ca56
heha-proglea <heha965+proglea@gmail.com>
2018-11-16 09:31:23 +0000

    Add comments

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var helmet = require('helmet');
+// --- app.js => Webアプリケーション自体を表すモジュール ---
 
+// 使用するnpmモジュールの読み込み
+var createError = require('http-errors'); // HTTPのエラーを作成するモジュール
+var express = require('express'); // Expressの本体
+var path = require('path'); // ファイルパスを扱う、Node.js標準モジュール
+var cookieParser = require('cookie-parser'); // Cookie解析モジュール
+var logger = require('morgan'); // コンソールにログを整形して出力するモジュール
+var helmet = require('helmet'); // セキュリティ用モジュール(Middleware)
+
+// Routerオブジェクトのモジュールを読み込んで、登録。
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var photosRouter = require('./routes/photos');
 
+// Applicationオブジェクトを作成
 var app = express();
+// use関数で使用するミドルウェア関数を設定(※ミドルウェア関数の実行は登録順なので、登録する順番に注意)
 app.use(helmet());
 
 // view engine setup
+// set関数でシステム全体に関わる設定を行う
+// テンプレートファイルの場所をディレクトリ'views'に指定
 app.set('views', path.join(__dirname, 'views'));
+// テンプレートエンジンをPugに設定
 app.set('view engine', 'pug');
 
+// use関数で使用するミドルウェア関数を設定(※ミドルウェア関数の実行は登録順なので、登録する順番に注意)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ルーティングオブジェクト(ハンドラ)に対するパスの登録？？
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/photos', photosRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-'use strict';
+// ----- Routerオブジェクトの記述 -----
+
 const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
+// (GETメソッドで)"/"にアクセスされた時に第二引数のコールバック関数を実行
 router.get('/', (req, res, next) => {
+  // render関数で、テンプレートviews/index.pugからHTML形式の文字列を作り(=レンダリング)、レスポンスに返す
   res.render('index', { title: 'Express' });
 });
 
+// Routerオブジェクト自身のモジュール化
 module.exports = router;
